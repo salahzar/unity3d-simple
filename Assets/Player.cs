a082c9bca245c9a4c4ecd39f5712b9a41e2caf20
Salahzar Stenvaag <salahzar@gmail.com>
2019-01-04 16:20:54 +0100

    minor modifications on script

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,28 +1,19 @@
 ﻿
 using UnityEngine;
-using System.Collections;
 
-// A very simplistic car driving on the x-z plane.
-
+// Script di movimento molto semplice che usa le frecce per muoversi lungo gli assi x e z
 public class Player : MonoBehaviour
 {
     public float speed = 10.0f;
-
     void Update()
     {
-        // Get the horizontal and vertical axis.
-        // By default they are mapped to the arrow keys.
-        // The value is in the range -1 to 1
+        // freccia in su e in giu' (verticale) associata al movimento di x 
         float translationx =  - Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        float translationy = 0;
-        if(Input.GetAxis("Jump") > 0)
-        {
-            translationy = 0.3f;
-        }
-        float translationz =  Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-
+        // freccia a sinistra destra (orizzontale) associata al movimento z
+        float translationz = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        // se premo spazio aumento la y che e' l'altezza
+        float translationy = (Input.GetAxis("Jump") > 0) ? 0.3f : 0;
         
         transform.Translate(translationx, translationy, translationz);
-
     }
 }
